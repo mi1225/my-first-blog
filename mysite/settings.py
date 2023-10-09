c3f8d88201d60cabb7bdc4b6fcd372d86cc746ec
mi1225 <tsuzumi1225@outlook.jp>
2023-10-10 00:00:26 +0900

    Modified templates to display posts from database.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a04==-u1np85oh@-qpc%zfiaw%aqq&=c#p3u8@0fydq@k2$10('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
